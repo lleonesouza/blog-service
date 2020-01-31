3b52b43520a4c2bbde176c7249bce007ca257173
lleonesouza <aliencode.host@gmail.com>
2020-01-31 12:36:45 -0300

    __import acess to sequelize and redis dbs

---
+++ src/entities/index.ts
@@ -0,0 +1,40 @@
+import ipRegex from "ip-regex";
+import sanitizeHtml from "sanitize-html";
+
+import validateUser from './user/user'
+import validadeProject from './project/projects'
+
+import buildMakeUser from "./user/user";
+import buildMakeSource from "./ip/source";
+import buildMakeProject from './project/projects'
+import Id from "../Id/index";
+import crypto from 'crypto'
+
+const isValidIp = (ip:any) => {
+  return ipRegex({ exact: true }).test(ip);
+};
+
+const sanitizeUser = (user:any) => {
+  validateUser(user)
+  // TODO: allow more coding embeds
+  return sanitizeHtml(user, {
+    allowedIframeHostnames: ["codesandbox.io", "repl.it"]
+  });
+};
+
+const sanitizeProjects = (project:any) => {
+  validadeProject(project)
+}
+
+const md5 = (data:any) => {
+  return crypto
+    .createHash("md5")
+    .update(data, "utf8")
+    .digest("hex");
+};
+
+const makeSource = buildMakeSource({ isValidIp });
+const makeProject = buildMakeProject({ Id, md5, sanitizeUser, makeSource });
+const makeUser = buildMakeUser({ Id, md5, sanitizeProjects, makeSource });
+
+export {makeSource, makeProject, makeUser};
--- src/uses-cases/index.ts
+++ src/uses-cases/index.ts
@@ -1,3 +1,19 @@
+import { makeSource, makeUser, makeProject } from "../entities/index";
+import {UserDB, ProjectDB} from '../db/index'
+
+import {makeGetUser} from "./users/index";
+import {makeCreateUser} from "./users/index";
+import {makeDeleteUser} from "./users/index";
+import {makeUpdateUser} from "./users/index";
+import {makeGetUsers} from './users/index'
+
+import {makeGetProject} from "./project/index";
+import {makeGetProjects} from "./project/index";
+import {makeCreateProject} from "./project/index";
+import {makeDeleteProject} from "./project/index";
+import {makeUpdateProject} from "./project/index";
+
+
 import makeGetStories from "./medium/getStories";
 import makeGetGithub from "./github/getGithub";
 import makeGetRepositories from "./github/getRepositories";
@@ -10,12 +26,56 @@ const RSS2JSON_URL = `https://api.rss2json.com/v1/api.json?rss_url=${MEDIUM_URL}
 const GITHUB_URL = `https://api.github.com/users/${USER}`;
 const GITHUB_REPOSITORIES_URL = `https://api.github.com/users/${USER}`;
 
+
+// User
+const createUser = makeCreateUser({makeUser, makeSource, UserDB});
+const updateUser = makeUpdateUser({makeUser, makeSource, UserDB});
+const deleteUser = makeDeleteUser({makeUser, makeSource, UserDB});
+const getUser = makeGetUser({makeUser, makeSource, UserDB});
+const getUsers = makeGetUsers({makeUser, makeSource, UserDB});
+
+// Projects
+const createProject = makeCreateProject({makeProject, makeSource, ProjectDB});
+const updateProject = makeUpdateProject({makeProject, makeSource, ProjectDB});
+const deleteProject = makeDeleteProject({makeProject, makeSource, ProjectDB});
+const getProject = makeGetProject({makeProject, ProjectDB});
+const getProjects = makeGetProjects({makeProject, ProjectDB});
+
+// Resources
 const getStories = makeGetStories(axios, RSS2JSON_URL);
 const getGithub = makeGetGithub(axios, GITHUB_URL);
 const getRepositories = makeGetRepositories(axios, GITHUB_REPOSITORIES_URL);
 
-const index = Object.freeze({ getStories, getGithub, getRepositories });
+const index = Object.freeze({
+  getStories,
+  getGithub,
+  getRepositories,
+  createUser,
+  updateUser,
+  deleteUser,
+  getUsers,
+  getUser,
+  createProject,
+  updateProject,
+  deleteProject,
+  getProject,
+  getProjects
+});
 
 export default index;
 
-export { getStories, getGithub, getRepositories };
+export {
+  getStories,
+  getGithub,
+  getRepositories,
+  createUser,
+  updateUser,
+  deleteUser,
+  getUsers,
+  getUser,
+  createProject,
+  updateProject,
+  deleteProject,
+  getProject,
+  getProjects
+};
